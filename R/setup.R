22922e7b4890e42b0745ff9caced35b854de5362
Kevin Kramer <kevin.pasqual.kramer@proton.me>
2023-10-04 16:10:15 +0200

    example_config.txt

--- R/setup.R
+++ R/setup.R
@@ -7,6 +7,7 @@
 #' @seealso [examples]
 #'
 #' @export
+#'
 setup_directory <- function(root = ".", include_examples = TRUE) {
   full_path <- path.expand(root)
   # Check if directory is empty
@@ -33,6 +34,9 @@ setup_directory <- function(root = ".", include_examples = TRUE) {
   }
 
   origin = system.file("extdata", package = "rmodvege")
+  origins = c()
+  destinations = c()
+
   # First, all files that go to `input/`
   sites = c("posieux", "sorens")
   input_types = c("weather.txt", 
@@ -46,8 +50,8 @@ setup_directory <- function(root = ".", include_examples = TRUE) {
   destination = file.path(full_path, "input/")
   for (input_file in input_files) {
     original = file.path(origin, input_file)
-    print(sprintf("Copying %s to %s...", original, destination))
-    file.copy(original, destination)
+    origins = c(origins, original)
+    destinations = c(destinations, destination)
   }
   # Then all data files
   subsites = c("1", "2")
@@ -56,9 +60,18 @@ setup_directory <- function(root = ".", include_examples = TRUE) {
     for (subsite in subsites) {
       data_filename = paste0(site, subsite, ".csv")
       original = file.path(origin, data_filename)
-      print(sprintf("Copying %s to %s...", original, destination))
-      file.copy(original, destination)
+      origins = c(origins, original)
+      destinations = c(destinations, destination)
     }
+  }
+  # Finally, an example config file
+  origins = c(origins, file.path(origin, "example_config.txt"))
+  destinations = c(destinations, file.path(full_path))
+  for (i in 1:length(origins)) {
+    original = origins[i]
+    destination = destinations[i]
+    print(sprintf("Copying `%s` to `%s`.", original, destination))
+    file.copy(original, destination)
   }
   print(sprintf("Copied example files to respective directories."))
 }
